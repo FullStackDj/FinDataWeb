1a243d80221c9f1145c4036cb81ad2119a36555f
FullStackDj <full.stackdj@gmail.com>
2025-06-24 10:49:44 +0300

    Added basic JSX structure and typed props in components

---
+++ src/Components/CardList/CardList.tsx
@@ -0,0 +1,16 @@
+import Card from "../Card/Card";
+
+interface Props {
+}
+
+const CardList = (props: Props) => {
+  return (
+    <div>
+      <Card companyName="test1" ticker="test2" price={100}/>
+      <Card companyName="test3" ticker="test4" price={101}/>
+      <Card companyName="test5" ticker="test6" price={102}/>
+    </div>
+  );
+};
+
+export default CardList;
