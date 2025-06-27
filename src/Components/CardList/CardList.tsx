9644b79a19a802e3eb5daeb60ffb7212064a0980
FullStackDj <full.stackdj@gmail.com>
2025-06-27 14:05:24 +0300

    Added onSubmit handlers to search and portfolio forms, replacing onClick

--- src/Components/CardList/CardList.tsx
+++ src/Components/CardList/CardList.tsx
@@ -1,18 +1,19 @@
-import {JSX} from "react";
+import {JSX, SyntheticEvent} from "react";
 import {v4 as uuidv4} from "uuid";
 import Card from "../Card/Card";
 import {CompanySearch} from "../../company";
 
 interface Props {
   searchResults: CompanySearch[];
+  onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const CardList: React.FC<Props> = ({searchResults}: Props): JSX.Element => {
+const CardList: React.FC<Props> = ({searchResults, onPortfolioCreate}: Props): JSX.Element => {
   return (
     <>
       {searchResults.length > 0 ? (
         searchResults.map((result) => {
-          return <Card id={result.symbol} key={uuidv4()} searchResult={result}/>
+          return <Card id={result.symbol} key={uuidv4()} searchResult={result} onPortfolioCreate={onPortfolioCreate}/>
         })
       ) : (
         <h1>No results</h1>
