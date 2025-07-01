8bb330abf31adc48ee4f2e266fc90b1855a103b0
FullStackDj <full.stackdj@gmail.com>
2025-07-01 11:29:23 +0300

    Implemented basic routing structure with React Router

---
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -0,0 +1,13 @@
+import {useParams} from "react-router-dom";
+
+interface Props {}
+
+const CompanyPage = (props: Props) => {
+  const { ticker } = useParams();
+
+  return (
+    <div>Company Page for: {ticker}</div>
+  );
+};
+
+export default CompanyPage;
