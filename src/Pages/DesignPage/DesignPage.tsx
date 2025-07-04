bd9f91a305c763b399b8cde13837db862957c7fa
FullStackDj <full.stackdj@gmail.com>
2025-07-04 08:03:37 +0300

    Added reusable table component and design guide page

---
+++ src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,15 @@
+import Table from "../../Components/Table/Table";
+
+type Props = {};
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+      <h1>FinData Design Page</h1>
+      <h2>This is FinData's design page.</h2>
+      <Table/>
+    </>
+  );
+};
+
+export default DesignPage;
