875f67d9ac64398a5917d3dd3da5e84d9aa73b15
FullStackDj <full.stackdj@gmail.com>
2025-07-06 10:31:04 +0300

    Added key metrics configuration for displaying company ratios

--- src/Components/CompanyProfile/CompanyProfile.tsx
+++ src/Components/CompanyProfile/CompanyProfile.tsx
@@ -1,4 +1,59 @@
+import {CompanyKeyMetrics} from "../../company";
+
 type Props = {};
+
+const tableConfig = [
+  {
+    label: "Market cap",
+    render: (company: CompanyKeyMetrics) => company.marketCapTTM,
+    subTitle: "Company’s total value",
+  },
+  {
+    label: "Current ratio",
+    render: (company: CompanyKeyMetrics) => company.currentRatioTTM,
+    subTitle: "Covers short-term debts",
+  },
+  {
+    label: "Return on equity",
+    render: (company: CompanyKeyMetrics) => company.roeTTM,
+    subTitle: "Profit from equity",
+  },
+  {
+    label: "Return on assets",
+    render: (company: CompanyKeyMetrics) => company.returnOnTangibleAssetsTTM,
+    subTitle: "Profit from assets",
+  },
+  {
+    label: "Free cashflow per share",
+    render: (company: CompanyKeyMetrics) => company.freeCashFlowPerShareTTM,
+    subTitle: "Free cash per share",
+  },
+  {
+    label: "Book value per share TTM",
+    render: (company: CompanyKeyMetrics) => company.bookValuePerShareTTM,
+    subTitle: "Net worth per share",
+  },
+  {
+    label: "Dividend yield TTM",
+    render: (company: CompanyKeyMetrics) => company.dividendYieldTTM,
+    subTitle: "Dividends vs. stock price",
+  },
+  {
+    label: "Capex per share TTM",
+    render: (company: CompanyKeyMetrics) => company.capexPerShareTTM,
+    subTitle: "Spending per share",
+  },
+  {
+    label: "Graham number",
+    render: (company: CompanyKeyMetrics) => company.grahamNumberTTM,
+    subTitle: "Max fair stock price",
+  },
+  {
+    label: "PE ratio",
+    render: (company: CompanyKeyMetrics) => company.peRatioTTM,
+    subTitle: "Price vs. earnings",
+  },
+];
 
 const CompanyProfile = (props: Props) => {
   return (
