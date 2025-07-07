76dd675cf6809543415945aaaf78baa75646438d
FullStackDj <full.stackdj@gmail.com>
2025-07-07 09:41:19 +0300

    Added income statement configs for rendering company financial metrics

--- src/Components/IncomeStatement/IncomeStatement.tsx
+++ src/Components/IncomeStatement/IncomeStatement.tsx
@@ -1,5 +1,62 @@
-type Props = {
-};
+import {CompanyIncomeStatement} from "../../company";
+
+type Props = {};
+
+const configs = [
+  {
+    label: "Date",
+    render: (company: CompanyIncomeStatement) => company.date,
+  },
+  {
+    label: "Revenue",
+    render: (company: CompanyIncomeStatement) => company.revenue,
+  },
+  {
+    label: "Cost of revenue",
+    render: (company: CompanyIncomeStatement) => company.costOfRevenue,
+  },
+  {
+    label: "Depreciation",
+    render: (company: CompanyIncomeStatement) =>
+      company.depreciationAndAmortization,
+  },
+  {
+    label: "Operating income",
+    render: (company: CompanyIncomeStatement) => company.operatingIncome,
+  },
+  {
+    label: "Income before taxes",
+    render: (company: CompanyIncomeStatement) => company.incomeBeforeTax,
+  },
+  {
+    label: "Net income",
+    render: (company: CompanyIncomeStatement) => company.netIncome,
+  },
+  {
+    label: "Net income ratio",
+    render: (company: CompanyIncomeStatement) => company.netIncomeRatio,
+  },
+  {
+    label: "Earnings per share",
+    render: (company: CompanyIncomeStatement) => company.eps,
+  },
+  {
+    label: "Earnings per diluted",
+    render: (company: CompanyIncomeStatement) => company.epsdiluted,
+  },
+  {
+    label: "Gross profit ratio",
+    render: (company: CompanyIncomeStatement) => company.grossProfitRatio,
+  },
+  {
+    label: "Operating income ratio",
+    render: (company: CompanyIncomeStatement) => company.operatingIncomeRatio,
+  },
+  {
+    label: "Income before taxes ratio",
+    render: (company: CompanyIncomeStatement) => company.incomeBeforeTaxRatio,
+  },
+];
 
 const IncomeStatement = (props: Props) => {
   return (
