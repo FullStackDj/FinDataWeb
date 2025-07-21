4a2d2b5213511ecfe050ad36ace7e8389db3d85e
FullStackDj <full.stackdj@gmail.com>
2025-07-21 11:48:30 +0300

    Added logout button and related logic

--- src/Routes/ProtectedRoute.tsx
+++ src/Routes/ProtectedRoute.tsx
@@ -1,7 +1,6 @@
-import {useLocation} from "react-router-dom";
-import {useAuth} from "../Context/useAuth";
-import {Navigate} from "react-router-dom";
 import {ReactNode} from "react";
+import {Navigate, useLocation} from "react-router-dom";
+import {useAuth} from "../Context/useAuth";
 
 type Props = { children: ReactNode };
 
