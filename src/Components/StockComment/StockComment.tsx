65d432c3985b928282fce5e728685436896c6628
FullStackDj <full.stackdj@gmail.com>
2025-07-23 08:43:17 +0300

    Added comment list fetching and display components

--- src/Components/StockComment/StockComment.tsx
+++ src/Components/StockComment/StockComment.tsx
@@ -1,6 +1,10 @@
+import {useEffect, useState} from "react";
 import {toast} from "react-toastify";
 import StockCommentForm from "./StockCommentForm/StockCommentForm";
-import {commentPostAPI} from "../../Services/CommentService";
+import {commentGetAPI, commentPostAPI} from "../../Services/CommentService";
+import {CommentGet} from "../../Models/Comment";
+import Spinner from "../Spinner/Spinner";
+import StockCommentList from "../StockCommentList/StockCommentList";
 
 type Props = {
   stockSymbol: string;
@@ -12,17 +16,38 @@ type CommentFormInputs = {
 }
 
 const StockComment = ({stockSymbol}: Props) => {
+  const [comments, setComments] = useState<CommentGet[] | null>(null);
+  const [loading, setLoading] = useState<boolean>();
+
+  useEffect(() => {
+    getComments();
+  }, []);
+
   const handleComment = (e: CommentFormInputs) => {
     commentPostAPI(e.title, e.content, stockSymbol).then((res) => {
       if (res) {
         toast.success("Comment created successfully");
+        getComments();
       }
     }).catch((e) => {
       toast.warning(e);
     })
 
   }
-  return <StockCommentForm symbol={stockSymbol} handleComment={handleComment}/>;
+
+  const getComments = () => {
+    setLoading(true);
+    commentGetAPI(stockSymbol).then((res) => {
+      setLoading(false);
+      setComments(res?.data!);
+    })
+  }
+  return (
+    <div className="flex flex-col">
+      {loading ? <Spinner/> : <StockCommentList comments={comments!}/>}
+      <StockCommentForm symbol={stockSymbol} handleComment={handleComment}/>
+    </div>
+  );
 };
 
 export default StockComment;
