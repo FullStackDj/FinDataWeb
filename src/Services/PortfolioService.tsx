3b7030de7edf689a74e2e5c10ebedc467cb09351
FullStackDj <full.stackdj@gmail.com>
2025-07-24 10:19:39 +0300

    Implemented portfolio API service with axios for CRUD operations

---
+++ src/Services/PortfolioService.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import {PortfolioGet, PortfolioPost} from "../Models/Portfolio";
+import {handleError} from "../Helpers/ErrorHandler";
+
+const api = "http://localhost:5078/api/portfolio/";
+
+export const portfolioAddAPI = async (symbol: string) => {
+  try {
+    const data = await axios.post<PortfolioPost>(api + `?symbol=${symbol}`);
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export const portfolioDeleteAPI = async (symbol: string) => {
+  try {
+    const data = await axios.delete<PortfolioPost>(api + `?symbol=${symbol}`);
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export const portfolioGetAPI = async () => {
+  try {
+    const data = await axios.get<PortfolioGet[]>(api);
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export const portfolioUpdateAPI = async (symbol: string, updateData: Partial<PortfolioPost>) => {
+  try {
+    const data = await axios.put<PortfolioPost>(api + `?symbol=${symbol}`, updateData);
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
